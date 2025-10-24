3249517ff309178c26d12b1626632a0b24909cd2
falahsab <40568313+falahsab@users.noreply.github.com>
2025-10-24 16:23:50 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,22 @@
-self.addEventListener("fetch", ()=> {});
+const CACHE_NAME = 'jedwal-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/css/style.css',
+  '/js/main.js',
+  '/img/yemen-sat.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
