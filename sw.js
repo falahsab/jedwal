368bb2af915a2ba5a692b3f6c600b7ffa94b8535
falahsab <40568313+falahsab@users.noreply.github.com>
2025-10-24 17:37:27 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,21 +2,40 @@ const CACHE_NAME = 'jedwal-cache-v1';
 const urlsToCache = [
   '/',
   '/index.html',
-  '/css/style.css',
-  '/js/main.js',
-  '/img/yemen-sat.png'
+  '/css/styles.css',
+  '/js/script.js',
+  '/fonts/Tajawal-Regular.woff2',
+  '/fonts/Tajawal-Regular.woff',
+  '/fonts/Tajawal-Bold.woff2',
+  '/fonts/Tajawal-Bold.woff',
+  'https://code.jquery.com/jquery-3.6.0.min.js',
+  'https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css',
+  'https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }))
+    )
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
